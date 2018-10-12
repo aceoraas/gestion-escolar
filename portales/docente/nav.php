79754d76582fba3cca0bc90c3dada2fe293a721c
maindeveloperpc <acerocol67gmail.com>
2018-10-12 16:28:52 -0400

    modificacion hoy
    aaa

--- portales/docente/nav.php
+++ portales/docente/nav.php
@@ -1,21 +1,26 @@
+
+
 <ul id="dropdown1" class="dropdown-content">
-  <li><a href="#!">Buscardor</a></li>
-  <li class="divider"></li>
-  <li><a href="./">Asistencia</a></li>
-  <li><a href="Resumen.php">Resumen</a></li>
+<li><a href="#!">Buscardor</a></li>
+<li class="divider"></li>
+<li><a href="./" ">Asistencia</a></li>
+<li><a  href="Resumen.php">Resumen</a></li>
+  
 </ul>
+
+
 <div class="navbar-fixed">
 <nav>
   <div class="nav-wrapper teal lighten-2 z-depth-3">
   	<a id='nameuser' style="margin-left: 20px" href="../perfil.php"></a>
   	<a style="margin-left: 20px" href="#"><?php $fecha=date('d/m/Y'); echo $fecha;?></a>
     <a href="#!" class="brand-logo center">U.B.A.C.B</a>
-    <ul class="right hide-on-med-and-down">
+    <ul id='menu1' class="right hide-on-med-and-down">
     <!-- Dropdown Trigger -->
-      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li><a  class="dropdown-button" href="#!" data-activates="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
     
-      <li><a href="#">Chat</a></li>
-      <li><a href="../../procesos/salida.php">Salir</a></li>
+      <li><a  href="#">Chat</a></li>
+      <li><a id="btnsalida" href="../../procesos/salida.php">Salir</a></li>
     </ul>
   </div>
 </nav>
@@ -36,7 +41,9 @@
    // Displays dropdown with edge aligned to the left of button
 });
   });
-
+$('#btnsalida').on('click',function(){
+  sessionStorage.clear();
+});
 $(document).ready(function(){
   var userdate= JSON.parse(sessionStorage.misdatosusuario);
   document.getElementById('nameuser').innerHTML=userdate.usuario;
--- portales/perfil.php
+++ portales/perfil.php
@@ -1,3 +1,7 @@
+<?php
+    $_GET['n'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,99 +17,133 @@
 			color: #FFF;
 		}
 	</style>
-	<title>Fulano</title>
+	<title id="title"></title>
 </head>
 <body >
-	<div class="navbar-fixed">
-<nav>
-  <div class="nav-wrapper teal lighten-2 z-depth-3">
-  	<a style="margin-left:20px" href="#!">USER-DATE</a>
-  	<a href="#!" style="margin-left:20px"><?php $fecha=date('d/m/Y'); echo $fecha;?> </a>
-    <a href="#!" class="brand-logo center">U.B.A.C.B</a>
-    <ul class="right hide-on-med-and-down">
-    <!-- Dropdown Trigger -->
-      <li><a class="dropdown-button" href="#!" data-activates="dropdown1">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
-    
-      <li><a href="#">Chat<span class="new badge red" data-badge-caption="mensajes">56</span></a></li>
-      <li><a href="../procesos/salida.php">Salir</a></li>
-    </ul>
-  </div>
-</nav>
-</div>
-
+<?php
+$f=base64_decode($_GET['n']);
+include './'.$f.'/nav.php';
+?>
 <!--fin de nav-->
 <!--inicio del body-->
 <center>
-<div class="col s12 m7">
-    <div class="card horizontal black-text" style="width: 98%; height: 90%;">
+<div class="row">
+<br>
+    <div class="horizontal black-text" style="width: 98%;">
+      
+      <div class="col s12 l4 card-content"  style="height: 90%;background-color: white; padding-bottom: 1em; padding-top: 1em; border-radius: 5px;">
+
+      <img class="circle responsive-img z-depth-4" width="128" height="128" src="../assets/img/alumnos/pes.jpg">
       <div class="card-content">
-      <img class="circle responsive-img z-depth-4" width="300" height="600" src="../../assets/img/alumnos/pes.jpg">
-      <div class="card-content">
-                	<p style="margin-top: 3%;">
-             		<h6><b>_id:</b> 5b3e96aab832b1454e6c9bdc</h6><br>
-					<h6><b>Cedula:</b> V-27226407</h6><br>
-					<h6><b>Nombre:</b> Fulano Alberto</h6><br>
-					<h6><b>Apellido:</b> De Tal</h6><br>
-					<h6><b>Fecha_Nacimiento:</b> 1999/02/06</h6><br>
-					<h6><b>Sexo:</b> Masculino</h6><br>
-					<h6><b>Edad:</b> 19 años</h6><br>
-					<h6><b>Area Laboral:</b> Departamento de Informatica</h6><br>
-                   	</p>
+          <div id='dataus' style="margin-top: 3%;"></div>
       </div>
       </div>
-      <div class="card-stacked grey lighten-3" >
-        <div class="card-content">
-        	<form autocomplete="on"  action="" method="post" accept-charset="utf-8" >
+
+      <div class="col s12 l8 card-stacked grey lighten-3" style="padding-bottom: 3.75em; padding-top: 1em; border-radius: 5px ;">  
+      <div class="card-content">
+        	
                 <!--Cuenta-->
                 <!--Usuario-->
-                <div class="input-field col s12 m12 l12">
+                <div class="input-field col s12">
                 <i class="material-icons prefix">account_circle</i>
-                <input disabled name="usuario" type="text" value="raas">
+                <input id='usernameform' disabled name="usuario" type="text">
                 </div>
                 <!--Contraseña-->
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 ">
                 <i class="material-icons prefix">vpn_key</i>
                 <input name="contraconfir" type="password" class="validate"data-length="8">
                 <label for="password">Contraseña Actual</label>
                 </div>
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 l6">
                 <i class="material-icons prefix">dialpad</i>
                 <input name="contrasenia" type="password" class="validate" data-length="8">
                 <label for="contrasenia">Contraseña nueva</label>
                 </div>
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 l6">
                 <i class="material-icons prefix">dialpad</i>
                 <input name="contraconfir" type="password" class="validate"data-length="8">
                 <label for="password">Confirme la contraseña</label>
                 </div>
                 <!--BOTON CAMBIAR CONTRASEÑA-->
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12">
                 <input class="btn "type="submit" name="Contraseña" value="Cambiar contraseña">
                 </div>
                 <!--datos personales-->
                 <!--Correo-->
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12">
                 <i class="material-icons prefix">email</i>
                 <input name="correo" type="email" class="validate">
                 <label for="email">Correo</label>
                 </div>
                <!--Numero de contacto-->
-               <div class="input-field col s12 m6 l6">
+               <div class="input-field col s12 l6">
                <i class="material-icons prefix">phone</i>
                <input name="tel_fijo" type="tel" class="validate" data-length="11">
                <label for="Number of Phone">Telefono Fijo</label>
                </div>
-               <div class="input-field col s12 m6 l6">
+               <div class="input-field col s12 l6">
                <i class="material-icons prefix">phone_iphone</i>
                <input name="tel_movil" type="tel" class="validate" data-length="11">
                <label for="Number of Phone">Telefono Movil</label>
                </div>        		              
-        	</form>
-           <a href="profesorresumen.php" class="btn btn">Volver</a>
+        
+           <a id='salida2' href="./<?php echo $f; ?>/" class="btn btn">Volver</a>
+           
         </div>      
       </div>
     </div>
   </div>
   </center>
 </body>
+
+<script type="text/javascript">
+  //.... variables.......................
+  var obj= JSON.parse(localStorage.misdatos);
+  var objse= JSON.parse(sessionStorage.misdatosusuario);
+  $('#title').html(objse.usuario+' - '+ obj.CARGO.TIPO);
+  
+
+//................datos de usuario lateral izquierdo........
+  $('#dataus').html('<p><h6><b>_id: '+obj._id.$oid+'</b></h6><br><h6><b>Cedula: '+objse.id_u+'</b></h6><br><h6><b>Nombre: '+obj.PNombre+' '+obj.SNombre+'</b></h6><br><h6><b>Apellido: '+obj.PApellido+' '+obj.SApellido+'</b></h6><br><h6><b>Fecha_Nacimiento: '+obj.Fecha_Nacimiento+'</b></h6><br><h6><b>Sexo: '+obj.Sexo+'</b></h6><br><h6><b>Edad: '+calcularEdad(obj.Fecha_Nacimiento)+' años </b></h6><br><h6><b>Area Laboral: '+obj.CARGO.TIPO+' '+obj.CARGO.AREA+'</b></h6></p>');
+
+  function calcularEdad(fecha) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    return edad;
+}
+
+
+//..............Datos de ususarios formulario............
+$('#usernameform').val(objse.usuario);
+
+
+
+
+ //......................................
+   $('#menu1').hide();
+   $('#nameuser').hide();
+
+  //......................................
+  var dir= obj.CARGO.TIPO;
+  dir = dir.toLowerCase();
+  var geti=btoa(unescape(encodeURIComponent(dir)));
+  
+  var a=window.location.href+'?n='+geti; 
+  
+  if (sessionStorage.laodperfil!='2'){
+    window.location=a;
+    sessionStorage.laodperfil='2';
+   }
+  
+  $('#salida2').on('click',function() {
+    sessionStorage.laodperfil='1';
+  });
+</script>
 </html>
